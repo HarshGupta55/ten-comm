88a638b3745a4a9e4bd8b622332d8ea6a4ac7e4c
HarshGupta55 <hk15005@gmail.com>
2025-06-12 11:00:14 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button>Button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am a input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <textarea placeholder="I am a text area" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+     </div>
+    </div>
+    
+  );
+}
